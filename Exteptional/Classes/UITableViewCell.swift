07a54552a8ecad809d6f2580361d2239dc84070d
Matteo Crippa <matteo@boostco.de>
2017-06-12 10:40:05 +0200

    better tintable

--- Exteptional/Classes/UITableViewCell.swift
+++ Exteptional/Classes/UITableViewCell.swift
@@ -10,11 +10,12 @@ import UIKit
 extension UITableViewCell {
   
   /// Enables disclosure to be tintable
-  public func disclosureTintable() {
+  public func tintDisclosure(with color: UIColor) {
     for case let button as UIButton in subviews {
       let image = button.backgroundImage(for: .normal)
       image?.withRenderingMode(.alwaysTemplate)
       button.setBackgroundImage(image, for: .normal)
+      button.tintColor = color
     }
   }
 }
