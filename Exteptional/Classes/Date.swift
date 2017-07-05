0b0e744bc2985145dc23cdfd3035d2a31060bc5f
Matteo Crippa <matteo@boostco.de>
2017-07-05 14:49:00 +0200

    date formatter

---
+++ Exteptional/Classes/Date.swift
@@ -0,0 +1,17 @@
+//
+//  Date.swift
+//  Exteptional
+//
+//  Created by Matteo Crippa on 05/07/2017.
+//
+
+import Foundation
+
+extension Date {
+  func toString(dateFormat format: String ) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = format
+    return dateFormatter.string(from: self)
+  }
+  
+}
