1bcefbbc61339f57ad2a7a2106a6becdd10c1f84
Matteo Crippa <matteo@boostco.de>
2017-07-05 14:55:51 +0200

    func need to be public under pod

--- Exteptional/Classes/Date.swift
+++ Exteptional/Classes/Date.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Date {
-  func toString(dateFormat format: String ) -> String {
+  public func toString(dateFormat format: String ) -> String {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = format
     return dateFormatter.string(from: self)
