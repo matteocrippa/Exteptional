f1d339bb6d25b5863b8a0dcd0e207f7b9b8a089b
Matteo Crippa <matteo@boostco.de>
2017-09-15 21:42:39 +0200

    added date to string

--- Exteptional/Classes/Date.swift
+++ Exteptional/Classes/Date.swift
@@ -9,6 +9,14 @@
 import Foundation
 
 extension Date {
+  
+  /// Easy convert Date to string with format
+  public func toString(dateFormat format: String ) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = format
+    return dateFormatter.string(from: self)
+  }
+  
   /// Check current date is now or in the future
   public var isFuture: Bool {
     return self >= Date()
